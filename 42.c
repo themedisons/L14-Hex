1568febc607303be6f3d1290a8e0eb4f10f5d8a2
themedisons <58108744+themedisons@users.noreply.github.com>
2020-01-18 10:46:48 +0200

    Create 42.c

---
+++ 42.c
@@ -0,0 +1,29 @@
+/* Programma 42.c
+Masīva kārtošana 
+Sandis Mālnieks 193RIC058 NEMB0
+*/
+
+#include <stdio.h>
+
+int main(){
+
+	int i, j, k; // ciklu mainīgie
+	int Temp; // īslaicīgais mainīgais
+	char mas3[]= "Sandis Malnieks"; // kārtojamo skaitļu masīvs
+
+	printf("Masīva izmērs = %ld\n", sizeof(mas3)/sizeof(mas3[0]));
+
+	for (i=0; i<(sizeof(mas3)/sizeof(mas3[0])); i++){ // cikliska masīva kārtošana ar burbuli
+		for (j=0; j<(sizeof(mas3)/sizeof(mas3[0])-1); j++){ // nākošais cikls ir par vienu mazāks
+			if (mas3[j] > mas3[j+1]){
+				Temp = mas3[j];
+				mas3[j] = mas3[j+1];
+				mas3[j+1] = Temp;
+			}
+		}
+	}
+// sakārtotā masīva izvade uz ekrāna
+	for(k=0; k<(sizeof(mas3)/sizeof(mas3[0])); k++){
+		printf("0x%02X = \"%c\"\n",mas3[k], mas3[k]);
+	}
+}
